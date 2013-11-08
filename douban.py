99d80e83916933e2bb8099e81f5733311bdbbd30
picasso250 <cumt.xiaochi@gmail.com>
2013-11-08 20:05:42 +0800

    不需要重复登录

--- douban.py
+++ douban.py
@@ -22,6 +22,22 @@ class PrivateFM(object):
         self.merge_cookie(cookie)
     
     def login(self, username, password):
+        if self.remember_cookie():
+            self.login_from_cookie()
+        else:
+            self.login_from_net(username, password)
+
+    def remember_cookie(self):
+        return 'dbcl2' in self.cookie and 'bid' in self.cookie
+
+    def login_from_cookie(self):
+        dbcl2 = self.cookie['dbcl2'].value
+        if dbcl2 and len(dbcl2) > 0:
+            self.dbcl2 = dbcl2
+            self.uid = self.dbcl2.split(':')[0]
+        self.bid = self.cookie['bid'].value
+
+    def login_from_net(self, username, password):
         print u'正在登录...'
         data = {
                 'source': 'radio',
@@ -30,39 +46,34 @@ class PrivateFM(object):
                 'remember': 'on',
                 'task': 'sync_channel_list'
                 }
-        # captcha_id = self.get_captcha_id()
-        # captcha = self.get_captcha_solution(captcha_id)
-        # data['captcha_id'] = captcha_id
-        # data['captcha_solution'] = captcha
-        # data = urllib.urlencode(data)
+        captcha_id = self.get_captcha_id()
+        captcha = self.get_captcha_solution(captcha_id)
+        data['captcha_id'] = captcha_id
+        data['captcha_solution'] = captcha
+        data = urllib.urlencode(data)
         print 'login ...'
         with closing(self.get_fm_conn()) as conn:
-            # headers = self.get_headers_for_request({
-            #     'Origin': 'http://douban.fm',
-            #     'Content-Type': 'application/x-www-form-urlencoded',
-            # })
-            # conn.request("POST", "/j/login", data, headers)
-            # response = conn.getresponse()
+            headers = self.get_headers_for_request({
+                'Origin': 'http://douban.fm',
+                'Content-Type': 'application/x-www-form-urlencoded',
+            })
+            conn.request("POST", "/j/login", data, headers)
+            response = conn.getresponse()
 
-            # set_cookie = response.getheader('Set-Cookie')
-            # if not set_cookie is None:
-            #     cookie = SimpleCookie(set_cookie)
-            #     self.save_cookie(cookie)
+            set_cookie = response.getheader('Set-Cookie')
+            if not set_cookie is None:
+                cookie = SimpleCookie(set_cookie)
+                self.save_cookie(cookie)
 
-            # print response.status
-            # body = response.read();
-            body = '{"user_info":{"ck":"0-jp","play_record":{"fav_chls_count":7,"liked":418,"banned":100,"played":9954},"is_new_user":0,"uid":"xiaochi2","third_party_info":null,"url":"http:\/\/www.douban.com\/people\/xiaochi2\/","is_dj":false,"id":"2778286","is_pro":false,"name":"小池·水"},"r":0}'
+            print response.status
+            body = response.read();
             body = json.loads(body)
             if body['r'] != 0:
                 print 'login failed'
                 thread.exit()
                 return 
             print 'ok'
-            dbcl2 = self.cookie['dbcl2'].value
-            if dbcl2 and len(dbcl2) > 0:
-                self.dbcl2 = dbcl2
-                self.uid = self.dbcl2.split(':')[0]
-            self.bid = self.cookie['bid'].value
+            self.login_from_cookie()
 
     def get_captcha_solution(self, captcha_id):
         self.show_captcha_image(captcha_id)
@@ -165,27 +176,36 @@ class PrivateFM(object):
   
     def get_params(self, typename=None):
         params = {}
-        params['r'] = random.random()
+        params['r'] = ''.join(random.sample('0123456789abcdefghijklmnopqrstuvwxyz0123456789', 10))
         params['uid'] = self.uid
-        params['channel'] = '0' 
+        params['channel'] = '-3' 
+        params['from'] = 'mainsite'
         if typename is not None:
             params['type'] = typename
         return params
 
     def communicate(self, params):
         print 'communicate'
+        print '/j/mine/playlist'
         print params
         data = urllib.urlencode(params)
-        cookie = 'dbcl2="%s"; bid="%s"' % (self.dbcl2, self.bid)
-        header = {"Cookie": cookie}
         with closing(httplib.HTTPConnection("douban.fm")) as conn:
-            conn.request('GET', "/j/mine/playlist?"+data, None, header)
+            conn.request('GET', "/j/mine/playlist?"+data, None, self.get_headers_for_request())
             result = conn.getresponse().read()
             return result
 
     def playlist(self):
+        print 'fetching playlist ...'
         params = self.get_params('n')
         result = self.communicate(params)
+        print json.loads(result)['r']
+        songs = json.loads(result)['song']
+        for s in songs:
+            print "\t" + s['title']+' ['+s['artist']+"]\t",
+            if s['like']:
+                print u'♥'
+            else:
+                print u'x'
         return json.loads(result)['song']
      
     def del_song(self, sid, aid):
@@ -208,7 +228,6 @@ class PrivateFM(object):
         self.communicate(params)
         
     def get_cache(self, name, default = None):
-        print 'get cache', name
         file_name = self.get_cache_file_name(name)
         if not os.path.exists(file_name):
             return default
