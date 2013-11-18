7a1f096f75a1c33cbdc904e1926693352c1cb4b4
picasso250 <cumt.xiaochi@gmail.com>
2013-11-18 16:00:49 +0800

    加入一些注释

--- doubanfm.sh
+++ doubanfm.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# 如果报错时间不能正确解析 (does not match format '%a, %d-%b-%Y %H:%M:%S GMT')，请运行这个脚本
+# 为了解决cookie时间不能正常解析的问题。这里应该写在python代码里，而非sh脚本里 todo
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
+
 python doubanfm.py
